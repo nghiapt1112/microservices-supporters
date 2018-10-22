e976759717eccbabe08c253d07f371ebb9920e04
nghiapt-vnext <nghiapt@vnext.com.vn>
2018-10-22 10:39:57 +0700

    [Refactor] Docker-compose build images

--- install.sh
+++ install.sh
@@ -3,12 +3,8 @@
 
 set -e
 
-cd sup-auth/ 			note "Building auth...";  	./gradlew clean build -x test; cd -
-cd sup-config-server/ 		note "Building config...";  	./gradlew clean build -x test; cd -
-cd sup-discovery-server/ 	note "Building discovery...";  	./gradlew clean build -x test; cd -
-cd sup-edge-server/ 		note "Building edge...";  	./gradlew clean build -x test; cd -
-cd sup-monitor-dashboard/ 	note "Building monitor...";  	./gradlew clean build -x test; cd -
-cd sup-turbine/ 		note "Building turbine..."; 	./gradlew clean build -x test; cd -
-#cd sup-zipkin-server/ 		note "Building zipkin...";  	./gradlew clean build -x test; cd -
+./gradlew build -x test
 
-docker compose up
+cd sup-auth/                 note "Building auth...";          ./gradlew build -x test; cd -
+
+docker-compose build
