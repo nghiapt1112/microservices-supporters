1170a7bc4cb35d64ce2885f8173c7758d9b26fd3
nghiapt1112 <nghiapt1112@gmail.com>
2020-04-29 03:21:26 +0000

    [CONFIG-SERVER] add deploy script

--- sup-config-server/deploy.sh
+++ sup-config-server/deploy.sh
@@ -4,4 +4,5 @@ sleep 2
 echo 'building ...'
 ./gradlew build -x test
 echo 'deploying ...'
-sudo nohup java -jar ./build/libs/sup-config-server.jar > ./sup-config-server.log 2>&1 &
+sudo nohup java -jar -Dspring.output.ansi.enabled=ALWAYS ./build/libs/sup-config-server.jar > ./sup-config-server.log 2>&1 &
+tail -1000f ./sup-config-server.log 
