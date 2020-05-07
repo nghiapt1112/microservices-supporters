92e5412aad50723a92ae54d9255c584865f32854
nghiapt1112 <nghiapt1112@gmail.com>
2020-05-07 11:31:04 +0700

    update deploy scripts

--- sup-discovery-server/deploy/deploy.sh
+++ sup-discovery-server/deploy/deploy.sh
@@ -1,4 +1,4 @@
 echo 'Stoping old agent...'
-sudo fuser -n tcp -k 8761
+sudo fuser -n tcp -k 8761 || sleep 2
 echo 'Deploying...'
-sudo nohup java -jar ./build/libs/sup-discovery-server-0.0.1.jar > ./sup-eureka.log 2>&1 &
+sudo nohup java -jar ./sup-discovery-server-0.0.1.jar > ./sup-eureka.log 2>&1 &
