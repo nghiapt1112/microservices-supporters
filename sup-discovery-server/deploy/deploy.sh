d51a5156294ef7b6693a3a6631be629445c0b937
nghiapt1112 <nghiapt1112@gmail.com>
2020-05-07 11:25:19 +0700

    update deploy script

--- sup-discovery-server/deploy/deploy.sh
+++ sup-discovery-server/deploy/deploy.sh
@@ -1,6 +1,4 @@
-echo 'Granting permission to gradlew ...'
-sudo chmod +x ./gradlew
-echo 'Building  ...'
-sudo ./gradlew build -x test
+echo 'Stoping old agent...'
 sudo fuser -n tcp -k 8761
+echo 'Deploying...'
 sudo nohup java -jar ./build/libs/sup-discovery-server-0.0.1.jar > ./sup-eureka.log 2>&1 &
